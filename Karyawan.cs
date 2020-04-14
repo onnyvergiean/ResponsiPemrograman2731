61b6e64ab47e5a24a70a650f5e04cbedc26df4b7
onny <onny.s@students.amikom.ac.id>
2020-04-14 12:11:58 +0700

    Finaliasi Project

--- Karyawan.cs
+++ Karyawan.cs
@@ -4,14 +4,22 @@ public class Karyawan
 {
 	public int NIK { get; set; }
 	public string Nama { get; set; }
-	public int GajiBulanan { get; set; }
+	public double GajiBulanan { get; set; }
 
 
-	public Karyawan(int NIK, string Nama, float GajiBulanan)
+	public Karyawan(int NIK, string Nama, int GajiBulanan)
 	{
 		this.NIK = NIK;
 		this.Nama = Nama;
 		this.GajiBulanan = GajiBulanan;
+		if (GajiBulanan < 0)
+		{
+			this.GajiBulanan = 0;
+		}
+		else
+		{
+			this.GajiBulanan = GajiBulanan;
+		}
 	}
 
 }
--- Program.cs
+++ Program.cs
@@ -6,17 +6,27 @@ namespace ResponsiPemrograman2731
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Nama     : Onny Vergiean Saputra");
+            Console.WriteLine("NIM      : 19.11.2731");
+            Console.WriteLine();
+
             Karyawan kar1 = new Karyawan(190302123,"Paijo",300000);
-            Karyawan kar2 = new Karyawan(190302124, "Jono",200000);
+            Karyawan kar2 = new Karyawan(190302124,"Jono", 200000);
 
             Console.WriteLine("No \t NIK/Nama \t \t GajiBulanan ");
             Console.WriteLine("----------------------------------------------------------");
             Console.WriteLine("1 \t {0} {1} \t  {2}", kar1.NIK, kar1.Nama, kar1.GajiBulanan);
             Console.WriteLine("2 \t {0} {1} \t  {2}", kar2.NIK, kar2.Nama, kar2.GajiBulanan);
             Console.WriteLine();
-            Console.WriteLine("Asyikk Kenaikan Gaji 10%1!");
+            Console.WriteLine("Asyikk Kenaikan Gaji 10%!!");
+            Console.WriteLine();
+            Console.WriteLine("No \t NIK/Nama \t \t GajiBulanan ");
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("1 \t {0} {1} \t  {2}", kar1.NIK, kar1.Nama, kar1.GajiBulanan = kar1.GajiBulanan + (0.1 * kar1.GajiBulanan));
+            Console.WriteLine("2 \t {0} {1} \t  {2}", kar2.NIK, kar2.Nama, kar2.GajiBulanan = kar2.GajiBulanan + (0.1 * kar2.GajiBulanan));
             Console.ReadKey();
+
+           
         }
     }
 
