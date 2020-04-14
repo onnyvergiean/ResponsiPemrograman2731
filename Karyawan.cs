ed387e13bc5ff6661c25e5e96b332141d07045a9
onny <onny.s@students.amikom.ac.id>
2020-04-14 11:24:37 +0700

    inisialiasi program dan penambahan class serta property

---
+++ Karyawan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Karyawan
+{
+	public int NIK { get; set; }
+	public string Nama { get; set; }
+	public float GajiBulanan { get; set; }
+
+	public Karyawan(int NIK, string Nama, float GajiBulanan)
+	{
+		this.NIK = NIK;
+		this.Nama = Nama;
+		this.GajiBulanan = GajiBulanan;
+	}
+
+	public void InfoGaji()
+	{
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ResponsiPemrograman2731
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan = new Karyawan(19,"AKu",12);
+
+            Console.ReadKey();
+        }
+    }
+
+}
